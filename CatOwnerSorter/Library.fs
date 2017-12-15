c78ae1a1470d8564a4bdee629e15afbc053c833f
Ettienne Gous <egous@ie.com.au>
2017-12-15 17:58:53 +1100

    Refactor into smaller funcs

---
+++ CatOwnerSorter/Library.fs
@@ -0,0 +1,30 @@
+﻿namespace CatOwnerSorter
+module Datasource = 
+   open FSharp.Data
+
+   [<Literal>]
+   let apiUrl = "http://agl-developer-test.azurewebsites.net/people.json"
+   [<Literal>]
+   let petTypesToFind = "Cat"
+   type OwnersAndCats = JsonProvider<apiUrl> //Can get inferred type from this command but it makes DEV very slow as its keeps fetching and dynamically typing
+
+   let loadData = OwnersAndCats.Load(apiUrl)
+
+module BusinessLogic  =
+   let getGenders collection = collection //Group owners collection by gender and get unique genders
+                                  |> Seq.groupBy (fun (x: Datasource.OwnersAndCats.Root) -> x.Gender)
+                                  |> Seq.map (fun (key, values) -> key)
+                                  |> List.ofSeq
+
+   let filterGender collection gender = collection
+                                           |> Seq.filter(fun (owner: Datasource.OwnersAndCats.Root) -> owner.Gender = gender) //Reduce original collection for each gender, 
+
+   let sortCats catCollection = catCollection |> Seq.sort
+
+   let outputCats catCollection = for cat in catCollection do
+                                    printfn "%s" cat
+
+   let aggregateCats (ownersByGender:seq<Datasource.OwnersAndCats.Root>) = ownersByGender
+                                                                           |> Seq.collect(fun pet -> pet.Pets
+                                                                                                      |> Seq.filter(fun pet -> pet.Type = Datasource.petTypesToFind)
+                                                                                                      |> Seq.map(fun pet -> pet.Name))
--- CatOwnerSorter/Program.fs
+++ CatOwnerSorter/Program.fs
@@ -1,31 +1,19 @@
 ﻿open System
 open FSharp.Data
 open FSharp.Linq
+open CatOwnerSorter
 
 [<Literal>]
-let apiUrl = "http://agl-developer-test.azurewebsites.net/people.json"
-[<Literal>]
 let petTypesToFind = "Cat"
-type OwnersAndCats = JsonProvider<apiUrl> //Can get inferred type from this command but it makes DEV very slow as its keeps fetching and dynamically typing
 
 [<EntryPoint>]
 let main argv =
-    let responseData = OwnersAndCats.Load(apiUrl)
-    //Group owners collection by gender and get unique genders
-    let genders = responseData
-                   |> Seq.groupBy (fun x -> x.Gender)
-                   |> Seq.map (fun (key, values) -> key)
-                   |> List.ofSeq
-
-    for gender in genders do
+    let data = Datasource.loadData
+    let genders = data |> BusinessLogic.getGenders
+    for gender in genders do 
        printfn "%s" gender
-       let petsForGender = responseData
-                              |> Seq.filter(fun owner -> owner.Gender = gender) //Reduce original collection for each gender, 
-                              |> Seq.collect(fun owner -> owner.Pets //aggregate the many results with collect, Its like SelectMany in Linq
-                                                          |> Seq.filter(fun pet -> pet.Type = petTypesToFind) //filter them by cats, 
-                                                          |> Seq.map(fun pet -> pet.Name)) //Get the name of each cat
-                              |> Seq.sort
-       for cat in petsForGender do
-          Console.WriteLine(cat)
+       let ownersByGender = BusinessLogic.filterGender data gender
+       BusinessLogic.aggregateCats ownersByGender |> BusinessLogic.sortCats |> BusinessLogic.outputCats
+       printfn ""
     let pause = Console.ReadLine()
     0 // return an integer exit code
