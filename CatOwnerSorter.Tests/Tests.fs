eba120b0722c0165a5b9e3c4f4eeccfde4f77169
Ettienne Gous <egous@ie.com.au>
2017-12-18 09:41:43 +1100

    More tests

--- CatOwnerSorter.Tests/Tests.fs
+++ CatOwnerSorter.Tests/Tests.fs
@@ -6,7 +6,7 @@ open CatOwnerSorter
 
 
 [<Literal>]
-let testData = """[{"name": "Jack","gender": "Male","age": 20,"pets": [{"name": "Garfield","type": "Cat"},{"name": "Fido","type": "Dog"}]},{"name": "Jessica","gender": "Female","age": 21,"pets": [{"name": "Grumpy","type": "Cat"}]},{"name": "Joe","gender": "Not Specified","age": 22,"pets": [{"name": "Happy","type": "Cat"}]}]"""
+let testData = """[{"name": "Jack","gender": "Male","age": 20,"pets": [{"name": "Happy","type": "Cat"},{"name": "Fido","type": "Dog"}]},{"name": "Jessica","gender": "Female","age": 21,"pets": [{"name": "Grumpy","type": "Cat"}]},{"name": "Joe","gender": "Not Specified","age": 22,"pets": [{"name": "Garfield","type": "Cat"}]}]"""
 
 [<Test>]
 let ``My test`` () =
@@ -18,3 +18,23 @@ let ``Aggregate Distinct Genders`` () =
    let genders = data |> BusinessLogic.getGenders
    let expected:string list = ["Male"; "Female"; "Not Specified"]
    Assert.AreEqual(genders, expected)
+
+[<Test>]
+let ``Filter Gender`` () =
+   let data = Datasource.parseData testData
+   let filteredGenders = BusinessLogic.filterGender data "Not Specified"
+   Assert.AreEqual(1, filteredGenders |> Seq.length)
+
+[<Test>]
+let ``Sort Cats`` () =
+   let data = ["a"; "c"; "b"]
+   let sortedExpectedData = ["a"; "b"; "c"]
+   let sortedCats = BusinessLogic.sortCats data
+   Assert.AreEqual(sortedExpectedData, sortedCats)
+   
+[<Test>]
+let ``Aggregate Cats`` () =
+   let data = Datasource.parseData testData
+   let sortedExpectedData = ["Happy"; "Grumpy"; "Garfield"]
+   let aggregatedCats = BusinessLogic.aggregateCats data
+   Assert.AreEqual(sortedExpectedData, aggregatedCats)
